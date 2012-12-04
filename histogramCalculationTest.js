2026e55ea3f88d7d8039b1f75f1dc253e330b54b
Nikola Geneshki <ngeneshki@gmail.com>
2012-12-04 15:30:24 +0200

    color borders, histogram and color number
    calculating those things. First implementation. not done yet.

--- histogramCalculationTest.js
+++ histogramCalculationTest.js
@@ -1,12 +1,19 @@
-﻿module( "module", {
+﻿module("Calculating histogram.", {
   setup: function() {
-    var ctx, dw = 20, dh = 20, canvas = document.createElement("canvas");
+    var canvas, ctx, dw = 20, dh = 20;
+	if ($("#canvas").length > 1) {
+		$("#canvas").remove();
+	} else if ($("#canvas").length === 1) {
+		canvas = $("#canvas").get(0);
+	} else {
+		canvas = document.createElement("canvas"), greenPixelCount = 20*20, redPixelCount = 0, bluePixelCount = 0;
+	}
 	$(canvas).attr('id', 'canvas');
 	ok(canvas, "canvas is present");
 	ok(!!canvas.getContext, "canvas supports 2d context");
-	$("body").append(canvas);
-	
-	
+	if ($("#canvas").length == 0) {
+		$("body").append(canvas);
+	}
 	canvas.height=dh;
 	canvas.width=dw;
 	ctx = canvas.getContext('2d');
@@ -36,4 +43,27 @@ test("Extracts colours from canvas", function () {
 	equal(matrix[matrix.length-3], 0, 'Actual value equals expected');
 	equal(matrix[matrix.length-2], 255, 'Actual value equals expected');
 	equal(matrix[matrix.length-1], 0, 'Actual value equals expected');
+});
+test("Calculate color borders", function() {
+	var sections = 3, allColorCount = 16777215;
+	var colorBorders = imageProcessing().calculateColorBorders(sections);
+	equal(colorBorders[0], allColorCount/3, "First third calculated fine.");
+	equal(colorBorders[1], (allColorCount/3)*2, "Second third calculated fine.");
+});
+
+test("Injects colors", function() {
+	var green = [0, 255, 0], blue = [0, 0, 255];
+	var greenInjected = imageProcessing().injectColor(green[0], green[1], green[2]);
+	var blueInjected = imageProcessing().injectColor(blue[0], blue[1], blue[2]);
+	equal(greenInjected, 255*256, "Color green is injected correctly.");
+	equal(blueInjected, 255, "Color blue is injected correctly.");
+});
+
+test("Calculates histogram properly.", function() {
+	var matrix = imageProcessing().extractColorMatrix(canvas);
+	var colorResolution = 3, greenIndex = 1;
+	var histogram = imageProcessing().calculateHistogram(matrix, colorResolution);
+	equal(histogram[greenIndex], greenPixelCount, "Count of green pixel is well calculated.");
+	equal(histogram[redIndex], redPixelCount, "Count of red pixel is well calculated.");
+	equal(histogram[blueIndex], bluePixelCount, "Count of blue pixel is well calculated.");
 });
--- imageProcessing.js
+++ imageProcessing.js
@@ -1,5 +1,6 @@
 ﻿var imageProcessing = function () {
 	"use strict";
+	var totalNumberOfColors = 16777215; // FFFFFF
 	var publ = {
 		extractColorMatrix: function (canvas) {
 			var imagedata, i = 0, cmIndex = 0, pixels, colorMatrix;
@@ -14,6 +15,31 @@
 				cmIndex+=3;
 			}
 			return colorMatrix;
+		},
+		calculateHistogram: function(matrix, colorResolution) {
+			var i = 0, sectionBorders = this.calculateColorBorders(colorResolution);
+			var histogram = [], injectedColor;
+			for (i = 0; i < matrix.length; i += 3) {
+				injectedColor = injectColor(matrix[i], matrix[i+1], matrix[i+2]);
+				// inject the three values to one and compare with the borders.
+			}
+		},
+		calculateColorBorders: function(sections) {
+			var i = 0, sectionBorders = [];
+			var sectionLength = totalNumberOfColors / sections;
+			var sectionRemainder = totalNumberOfColors % sections;
+			for (i = 1; i < sections; i += 1) {
+				var sectionBorder = Math.floor(sectionLength * i);
+				var addition = sectionRemainder * i;
+				if (Math.floor(addition) > 0 && addition % Math.floor(addition) >= 1) {
+					sectionBorder += Math.floor(addition);
+				}
+				sectionBorders.push(sectionBorder);
+			}
+			return sectionBorders;
+		},
+		injectColor: function(red, green, blue) {
+			return red*256*256 + green*256 + blue;
 		}
 	};
 	return publ;
