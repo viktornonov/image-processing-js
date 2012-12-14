796cb73f790225b3192cb2abbda3ae47a31f0de5
Nikola Geneshki <ngeneshki@gmail.com>
2012-12-14 15:05:56 +0200

    requantification function added
    Also yet not needed function for hsl to rgb conversion. Not to forget
    about some moving of functions up and down the file. Close-enough lib is
    used for testing section length.

--- histogramCalculationTest.js
+++ histogramCalculationTest.js
@@ -1,12 +1,95 @@
-﻿module("Calculating histogram.", {
+﻿module("testing HSL to RGB and RGB to HSL");
+test("Transform HSL to RGB format", function() {
+	var redBlueBorderH = 300;
+	var redBlueBorderS = 100;
+	var redBlueBorderL = 50;
+	var magendaResult = imageProcessing().hsl2rgb({ H: redBlueBorderH, S: redBlueBorderS, L: redBlueBorderL});
+	QUnit.close(magendaResult.R, 255, 1, " Magenta: Red color converted fine.");
+	QUnit.close(magendaResult.G, 0, 1, "Magenta: Green color converted fine.");
+	QUnit.close(magendaResult.B, 255, 1, "Magenta: Blue color converted fine.");
+	var whiteResult = imageProcessing().hsl2rgb({ H: redBlueBorderH, S: redBlueBorderS, L: 100});
+	QUnit.close(whiteResult.R, 255, 1, "White: Red color converted fine.");
+	QUnit.close(whiteResult.G, 255, 1, "White: Green color converted fine.");
+	QUnit.close(whiteResult.B, 255, 1, "White: Blue color converted fine.");
+	var randomColorResult = imageProcessing().hsl2rgb({H: 303, S: 80, L: 35});
+	QUnit.close(randomColorResult.R, 160, 1, "Random Color: Red color converted fine.");
+	QUnit.close(randomColorResult.G, 18, 1, "Random Color: Green color converted fine.");
+	QUnit.close(randomColorResult.B, 153, 1, "Random Color: Blue color converted fine.");
+	var randomColorResult2 = imageProcessing().hsl2rgb({H: 112, S: 80, L: 37});
+	QUnit.close(randomColorResult2.R, 39, 1, "Random Color 2: Red color converted fine.");
+	QUnit.close(randomColorResult2.G, 170, 1, "Random Color 2: Green color converted fine.");
+	QUnit.close(randomColorResult2.B, 19, 1, "Random Color 2: Blue color converted fine.");
+});
+//test("Transform RGB to HSL fromat", function() {
+//	var 
+//});
+
+module("Requantifying the image", {
+	setup: function() {
+		
+	}, teardown: function() {
+		
+	}
+});
+test("Calculate color section's length", function() {
+	var colorSectionLength = imageProcessing().colorSectionLength(3);
+	QUnit.close(colorSectionLength, 128, 1, "Color Section is well calculated.");
+	colorSectionLength = imageProcessing().colorSectionLength(6);
+	QUnit.close(colorSectionLength, 64, 1, "Color Section is well calculated.");
+	colorSectionLength = imageProcessing().colorSectionLength(9);
+	QUnit.close(colorSectionLength, 32, 1, "Color Section is well calculated.");
+	colorSectionLength = imageProcessing().colorSectionLength(12);
+	QUnit.close(colorSectionLength, 16, 1, "Color Section is well calculated.");
+});
+test("Injects colors", function() {
+	var green = [0, 255, 0], blue = [0, 0, 255];
+	var greenCalculated = imageProcessing().calculateColorNumber(green[0], green[1], green[2]);
+	var blueCalculated = imageProcessing().calculateColorNumber(blue[0], blue[1], blue[2]);
+	equal(greenCalculated, 255*256, "Color green is Calculated correctly.");
+	equal(blueCalculated, 255, "Color blue is Calculated correctly.");
+});
+
+test("Requantifies the colors", function() {
+	var strangeGreen = [0, 107, 87], strangeBlue = [0, 33, 128], strangeRed = [211, 127, 0];
+	var requantifiedGreen = imageProcessing().requantify(3, strangeGreen),
+	requantifiedBlue = imageProcessing().requantify(3, strangeBlue),
+	requantifiedRed = imageProcessing().requantify(3, strangeRed);
+	
+	equal(requantifiedGreen[0], 64, "First Color: Red is requantified well with 3 bits");
+	equal(requantifiedGreen[1], 64, "First Color: Green is requantified well with 3 bits");
+	equal(requantifiedGreen[2], 64, "First Color: Blue is requantified well with 3 bits");
+	equal(requantifiedBlue[0], 64, "Second Color: Red is requantified well with 3 bits");
+	equal(requantifiedBlue[1], 64, "Second Color: Green is requantified well with 3 bits");
+	equal(requantifiedBlue[2], 192, "Second Color: Blue is requantified well with 3 bits");
+	equal(requantifiedRed[0], 192, "Third Color: Red is requantified well with 3 bits");
+	equal(requantifiedRed[1], 64, "Third Color: Green is requantified well with 3 bits");
+	equal(requantifiedRed[2], 64, "Third Color: Blue is requantified well with 3 bits");
+	
+	requantifiedGreen = imageProcessing().requantify(6, strangeGreen),
+	requantifiedBlue = imageProcessing().requantify(6, strangeBlue),
+	requantifiedRed = imageProcessing().requantify(6, strangeRed);
+	
+	
+	equal(requantifiedGreen[0], 32, "First Color: Red is requantified well with 6 bits");
+	equal(requantifiedGreen[1], 96, "First Color: Green is requantified well with 6 bits");
+	equal(requantifiedGreen[2], 96, "First Color: Blue is requantified well with 6 bits");
+	equal(requantifiedBlue[0], 32, "Second Color: Red is requantified well with 6 bits");
+	equal(requantifiedBlue[1], 32, "Second Color: Green is requantified well with 6 bits");
+	equal(requantifiedBlue[2], 160, "Second Color: Blue is requantified well with 6 bits");
+	equal(requantifiedRed[0], 224, "Third Color: Red is requantified well with 6 bits");
+	equal(requantifiedRed[1], 96, "Third Color: Green is requantified well with 6 bits");
+	equal(requantifiedRed[2], 32, "Third Color: Blue is requantified well with 6 bits");
+});
+module("Calculating histogram.", {
   setup: function() {
-    var canvas, ctx, dw = 20, dh = 20;
+    var canvas, ctx, dw = 20, dh = 20,
+	greenPixelCount = 20*20, redPixelCount = 0, bluePixelCount = 0
 	if ($("#canvas").length > 1) {
 		$("#canvas").remove();
 	} else if ($("#canvas").length === 1) {
 		canvas = $("#canvas").get(0);
 	} else {
-		canvas = document.createElement("canvas"), greenPixelCount = 20*20, redPixelCount = 0, bluePixelCount = 0;
+		canvas = document.createElement("canvas");
 	}
 	$(canvas).attr('id', 'canvas');
 	ok(canvas, "canvas is present");
@@ -25,6 +108,7 @@
 	ctx.fillRect(0, 0, dw, dh);
 	ok(!!ctx.getImageData, 'Context has getImageData() method');
   }, teardown: function() {
+	$("#canvas").remove();
     ok( true, "and one extra assert after each test" );
   }
 });
@@ -44,26 +128,10 @@ test("Extracts colours from canvas", function () {
 	equal(matrix[matrix.length-2], 255, 'Actual value equals expected');
 	equal(matrix[matrix.length-1], 0, 'Actual value equals expected');
 });
-test("Calculate color borders", function() {
-	var sections = 3, allColorCount = 16777215;
-	var colorBorders = imageProcessing().calculateColorBorders(sections);
-	equal(colorBorders[0], allColorCount/3, "First third calculated fine.");
-	equal(colorBorders[1], (allColorCount/3)*2, "Second third calculated fine.");
-});
-
-test("Injects colors", function() {
-	var green = [0, 255, 0], blue = [0, 0, 255];
-	var greenInjected = imageProcessing().injectColor(green[0], green[1], green[2]);
-	var blueInjected = imageProcessing().injectColor(blue[0], blue[1], blue[2]);
-	equal(greenInjected, 255*256, "Color green is injected correctly.");
-	equal(blueInjected, 255, "Color blue is injected correctly.");
-});
-
 test("Calculates histogram properly.", function() {
 	var matrix = imageProcessing().extractColorMatrix(canvas);
-	var colorResolution = 3, greenIndex = 1;
+	var colorResolution = 3, greenIndex = 1, redIndex = 0, blueIndex = 2;
 	var histogram = imageProcessing().calculateHistogram(matrix, colorResolution);
-	equal(histogram[greenIndex], greenPixelCount, "Count of green pixel is well calculated.");
-	equal(histogram[redIndex], redPixelCount, "Count of red pixel is well calculated.");
-	equal(histogram[blueIndex], bluePixelCount, "Count of blue pixel is well calculated.");
+	equal(histogram.length, 1, "There's appropriate number of colors in the calculated histogram.");
+	equal(histogram[0], greenPixelCount, "The histogram calculated correctly the number of green pixels.");
 });
--- imageProcessing.js
+++ imageProcessing.js
@@ -1,7 +1,91 @@
 ﻿var imageProcessing = function () {
 	"use strict";
+	
+	//var ColorCounter = function () {
+	//	var 
+	//	return {
+	//		
+	//	}
+	//}
+	
 	var totalNumberOfColors = 16777215; // FFFFFF
+	
+	var instantiateArray = function(elementsCount) {
+		var i = 0, resultArray = [];
+		for (i = 0; i < elementsCount; i += 1) {
+			resultArray[i] = 0;
+		}
+		return resultArray;
+	}
+	
+	var findMin = function(r, g, b) {
+		if (r < g) {
+			if (r < b) return r;
+		} else {
+			if (g < b) return g;
+		}
+		return b;
+	}
+	
+	var findMax = function(r, g, b) {
+		if (r > g) {
+			if (r > b) return r;
+		} else {
+			if (g > b) return g;
+		}
+		return b;
+	}
+	
+	
+	
 	var publ = {
+		calculateColorNumber: function(red, green, blue) {
+			return (red*256*256 + green*256 + blue);
+		},
+		hsl2rgb: function(hslObject) {
+			var H = hslObject.H / 360,
+			S = hslObject.S / 100,
+			L = hslObject.L / 100,
+			red, green, blue, _1, _2;
+
+			function Hue_2_RGB(v1, v2, vH) {
+				if (vH < 0) vH += 1;
+				if (vH > 1) vH -= 1;
+				if ((6 * vH) < 1) return v1 + (v2 - v1) * 6 * vH;
+				if ((2 * vH) < 1) return v2;
+				if ((3 * vH) < 2) return v1 + (v2 - v1) * ((2 / 3) - vH) * 6;
+				return v1;
+			}
+
+			if (S == 0) { // HSL from 0 to 1
+				red = L * 255;
+				green = L * 255;
+				blue = L * 255;
+			} else {
+				if (L < 0.5) {
+				  _2 = L * (1 + S);
+				} else {
+				  _2 = (L + S) - (S * L);
+				}
+				_1 = 2 * L - _2;
+
+				red = Math.round(255 * Hue_2_RGB(_1, _2, H + (1 / 3)));
+				green = Math.round(255 * Hue_2_RGB(_1, _2, H));
+				blue = Math.round(255 * Hue_2_RGB(_1, _2, H - (1 / 3)));
+			}
+
+			return { R: red, G: green, B: blue};
+		},
+//		rgb2hsl: function (rgbObject) {
+//			var min = findMin(rgbObject), max = findMax(rgbObject);
+//			var hue = 0, saturation = 0, luminace = (min + max) / 2;
+//			if (min != max) {
+//				if (L/255 < 0.5) {
+//					
+//				}
+//			}
+//			return {H: hue, S: saturation, L: luminace};
+//		},
 		extractColorMatrix: function (canvas) {
 			var imagedata, i = 0, cmIndex = 0, pixels, colorMatrix;
 			var context = canvas.getContext('2d');
@@ -16,30 +100,37 @@
 			}
 			return colorMatrix;
 		},
+		colorSectionLength: function(sections) {
+			var i = 0;
+			if (sections % 3 != 0 && sections / 3 > 8) {
+				return false;
+			}
+			var sectionsPerColor = Math.pow(2, sections / 3),
+			sectionLength = 256 / sectionsPerColor;
+			return Math.round(sectionLength);
+		},
+		requantify: function(sections, oldColor) {
+			var sectionLength = this.colorSectionLength(sections), 
+			previousColorSection = 0, requantifiedColor = [], i = 0, requantifiedIngredient = 0;
+			for(i = 0; i < 3; i += 1) {
+				previousColorSection = Math.floor(oldColor[i]/sectionLength);
+				requantifiedIngredient = previousColorSection*sectionLength + Math.round(sectionLength/2);
+				requantifiedColor.push(requantifiedIngredient);
+			}
+			return requantifiedColor;
+		},
 		calculateHistogram: function(matrix, colorResolution) {
-			var i = 0, sectionBorders = this.calculateColorBorders(colorResolution);
-			var histogram = [], injectedColor;
+			var i = 0, k = 0, sectionBorders = this.colorSectionLength(colorResolution);
+			var calculatedColor = 0, histogram = instantiateArray(sectionBorders.length + 1);
 			for (i = 0; i < matrix.length; i += 3) {
-				injectedColor = injectColor(matrix[i], matrix[i+1], matrix[i+2]);
-				// inject the three values to one and compare with the borders.
-			}
-		},
-		calculateColorBorders: function(sections) {
-			var i = 0, sectionBorders = [];
-			var sectionLength = totalNumberOfColors / sections;
-			var sectionRemainder = totalNumberOfColors % sections;
-			for (i = 1; i < sections; i += 1) {
-				var sectionBorder = Math.floor(sectionLength * i);
-				var addition = sectionRemainder * i;
-				if (Math.floor(addition) > 0 && addition % Math.floor(addition) >= 1) {
-					sectionBorder += Math.floor(addition);
+				for (k = 0; k < sectionBorders.length; k += 1) {
+					if (sectionBorders[k] > calculatedColor) {
+						histogram[k] += 1;
+						break;
+					}
 				}
-				sectionBorders.push(sectionBorder);
 			}
-			return sectionBorders;
-		},
-		injectColor: function(red, green, blue) {
-			return red*256*256 + green*256 + blue;
+			return histogram;
 		}
 	};
 	return publ;
